c3653fa43ddc5542da831dc82445f78bd779d3aa
Rahul Saroj <rahulsaroj1999.rs@gmail.com>
2025-08-22 10:55:28 +0530

    init: initial project setup with venv support

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game, and create game resoruces."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+    
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance and run a game.
+    ai = AlienInvasion()
+    ai.run_game()
